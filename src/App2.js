51f984f459bb4c3e2a2f2247ea49928e090e7c7d
huongvu2312 <calm.and.chaotic@gmail.com>
2020-01-15 01:12:51 +0100

    [FUNCTION] Add login and register component

---
+++ src/App2.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Create from './components/create.component';
+import Edit from './components/edit.component';
+import Index from './components/index.component';
+
+import './App.css';
+
+class App2 extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to={'/login'} className="navbar-brand">Bookie</Link>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link to={'/login'} className="nav-link">Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/register'} className="nav-link">Register</Link>
+                </li>
+              </ul>
+            </div>
+          </nav> <br />
+          <h2>Welcome to React CRUD Tutorial</h2> <br />
+          <Switch>
+            <Route exact path='/create' component={Create} />
+            <Route path='/edit/:id' component={Edit} />
+            <Route path='/index' component={Index} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+
+export default App2;
+
+
