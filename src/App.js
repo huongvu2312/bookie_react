793114635e9b0af2b25a495401c9504b5cf7960d
huongvu2312 <calm.and.chaotic@gmail.com>
2020-01-19 01:31:30 +0100

    [FUNCTION] delete unnecessary redirect function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Redirect } from "react-router";
 
 import Login from "./components/login.component";
 import Register from "./components/register.component";
